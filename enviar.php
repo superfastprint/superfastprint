c07ae5ebe539fb54fe759d4cace5dbcfb3c8bf39
superfastprint <88921376+superfastprint@users.noreply.github.com>
2021-08-14 15:17:13 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,10 +1,10 @@
 <?php
      $destino= "diegoxdeleon03@gmail.com";
-     $nombre =  $_Post["nombre"];
-     $correo =  $_Post["correo"];
-     $telefono =  $_Post["telefono"];
-     $mensaje =  $_Post["mensaje"];
-     $contenido = "Nombre: " . $nombre . "/nCorreo: " . $correo . "/nTelefono: " . $telefono . "/nMensaje> " . $mensaje;
+     $nombre =  $_POST["nombre"];
+     $correo =  $_POST["correo"];
+     $telefono =  $_POST["telefono"];
+     $mensaje =  $_POST["mensaje"];
+     $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono: " . $telefono . "\nMensaje> " . $mensaje;
      mail($destino,"Contacto", $contenido);
      header("Location:gracias.html");
 ?>
